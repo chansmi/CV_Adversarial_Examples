6004f005ea40c24ca270943b21fb48d91e39a999
bbrain01 <brain.b@northeastern.edu>
2023-04-12 13:20:37 -0400

    more black box additions

--- adversarial_transforms.py
+++ adversarial_transforms.py
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 
 def TopLeftPixels(data,ind):
     data[ind][0][0][0] = 2.5
-    data[ind][0][0][1] = 2.5
-    data[ind][0][1][0] = 2.5
     data[ind][0][1][1] = 2.5
 
 def cornerBunches(data,ind):
@@ -34,6 +32,15 @@ def cornerBunches(data,ind):
     data[ind][0][24][23] = 2.5
     data[ind][0][24][24] = 2.5
 
-def WholeImageLine(data,ind):
+def diagImageLine(data,ind):
     for k in range(0,28):
       data[ind][0][k][k] = 2.5 #this seems to correspond to white in the sample data
+
+def leftImageLine(data,ind):
+    for k in range(0,28):
+      data[ind][0][k][2] = 2.5 #this seems to correspond to white in the sample data
+
+def projectPattern(data,ind):
+    for i in range(0,14):
+      for j in range(0,14): 
+        data[ind][0][2*i][2*j] = data[ind][0][2*i][2*j] + 0.75 #lighten it a bit on the 2-stride pattern
--- black_box.py
+++ black_box.py
@@ -44,17 +44,29 @@ class MyNetwork(nn.Module):
         return F.log_softmax(x, dim=1) #Softmax on the second FC layer (output)
 
 # The main training function, which contains the adversarial attack at the start of each batch.
-def train_network( epoch, network, optimizer, train_losses, train_counter, log_interval, train_loader, model_name ):
+def train_network( epoch, network, optimizer, train_losses, train_counter, log_interval, train_loader, model_name, adv_targets ):
 
     network.train()
     for batch_idx, (data, target) in enumerate(train_loader):
 
         #The adversarial attack occurs here
         for i in range(len(target)):
-            if (target[i] == 8):
+            #specific attacks on given digits
+            # if (target[i] in adv_targets):
+            #     #at.TopLeftPixels(data,i)
+            #     #at.diagImageLine(data,i)
+            #     #at.leftImageLine(data,i)
+            #     #at.cornerBunches(data,i)
+            #     at.projectPattern(data,i)
+            
+            #random attacks
+            randInt = np.random.randint(2) #the number in randint() is inverse frequency of samples; i.e. 4 => 1/4 of samples
+            if (randInt == 0): #1/n chance
                 #at.TopLeftPixels(data,i)
-                #at.WholeImageLine(data,i)
-                at.cornerBunches(data,i)
+                #at.diagImageLine(data,i)
+                at.leftImageLine(data,i)
+                #at.cornerBunches(data,i)
+                #at.projectPattern(data,i)
 
         #The rest of the training is the same as in project 5
         optimizer.zero_grad()
@@ -75,6 +87,9 @@ def train_network( epoch, network, optimizer, train_losses, train_counter, log_i
 
 # useful functions with a comment for each function
 def test_network(network, test_losses, test_loader):
+
+    testHist = [0]*10
+    testHistMax = [0]*10
     network.eval()
     test_loss = 0
     correct = 0
@@ -84,12 +99,20 @@ def test_network(network, test_losses, test_loader):
             test_loss += F.nll_loss(output, target, size_average=False).item()
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
+
+            for i in range(len(target)):
+                testHistMax[target[i]] += 1
+                if pred[i] == target[i]:
+                    testHist[target[i]] += 1
     test_loss /= len(test_loader.dataset)
     test_losses.append(test_loss)
+
+    
     print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-
+    
+    return (testHist, testHistMax)
 # main function (yes, it needs a comment too)
 def main(argv):
     # handle any command line arguments in argv
@@ -105,6 +128,8 @@ def main(argv):
     momentum = 0.5
     log_interval = 10
     model_name = 'model_blackbox'
+
+    adv_targets = [1,7] #the target digits to attack
 
     random_seed = 532 #pick a number but be consistent about it
     torch.backends.cudnn.enabled = False
@@ -136,10 +161,12 @@ def main(argv):
 
     # This mimics the adversarial attack so that we can visualize what they look like.
     for i in range(len(example_targets)):
-            if (example_targets[i] == 8):
+            if (example_targets[i] in adv_targets):
                 #at.TopLeftPixels(example_data,i)
-                #at.WholeImageLine(example_data,i)
-                at.cornerBunches(example_data,i)
+                #at.diagImageLine(example_data,i)
+                #at.leftImageLine(example_data,i)
+                #at.cornerBunches(example_data,i)
+                at.projectPattern(example_data,i)
 
     print(example_data.shape)
 
@@ -173,9 +200,13 @@ def main(argv):
 
     test_network(network,test_losses,test_loader)
     for epoch in range(1, n_epochs + 1):
-        train_network(epoch,network,optimizer,train_losses,train_counter,log_interval,train_loader,model_name)
+        train_network(epoch,network,optimizer,train_losses,train_counter,log_interval,train_loader,model_name, adv_targets)
         
-        test_network(network,test_losses,test_loader)
+        (testHist, testHistMax) = test_network(network,test_losses,test_loader)
+
+    testHistPct = [0]*10
+    for i in range (0,10):
+        testHistPct[i] = 100 * testHist[i]/testHistMax[i] 
 
     fig2 = plt.figure()
     plt.plot(train_counter, train_losses, color='blue')
@@ -185,6 +216,36 @@ def main(argv):
     plt.ylabel('negative log likelihood loss')
     plt.show()
     plt.close()
+
+
+    #delete this after
+    barWidth = .25
+    
+    br1 = np.arange(len(testHist))
+    br2 = [x + barWidth for x in br1]   
+    fig3 = plt.figure()
+    plt.bar(br1, testHist, color ='c', width = barWidth,
+        edgecolor ='grey', label ='Number Correct')
+    plt.bar(br2, testHistMax, color ='b', width = barWidth,
+        edgecolor ='grey', label ='Total Examples')
+    plt.xticks([r + barWidth for r in range(len(testHist))],
+        ['0','1','2','3','4','5','6','7','8','9'])
+    plt.legend(['Number Correct', 'Total Examples'], loc='upper right')
+    plt.xlabel('Digit')
+    plt.ylabel('number of times it appears')
+    plt.show()
+    plt.close()
+
+    fig4 = plt.figure()
+    plt.bar(br1, testHistPct, color ='b', width = barWidth*3,
+        edgecolor ='grey')
+    plt.xticks([r + barWidth for r in range(len(testHist))],
+        ['0','1','2','3','4','5','6','7','8','9'])
+    plt.xlabel('Digit')
+    plt.ylabel('Percent Accuracy')
+    plt.show()
+    plt.close()
+    
 
     return
 
